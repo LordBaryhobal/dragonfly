5d646d12e84f189ba12f0d286c300a132efa7a47
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-06-29 12:17:40 +0200

    implemented Message types

--- src/dragonfly/message.py
+++ src/dragonfly/message.py
@@ -31,6 +31,14 @@ SUBSCRIBED = 5
 UNSUBSCRIBE = 6
 UNSUBSCRIBED = 7
 
+ORIGIN_SERVER = 0
+ORIGIN_CLIENT = 1
+
+FLAG_0 = 1
+FLAG_1 = 2
+FLAG_2 = 4
+FLAG_3 = 8
+
 class MessageType:
     def __init__(self, byte):
         self.origin = (byte & 0x80) >> 7
@@ -49,15 +57,36 @@ class MessageType:
         return n.to_bytes(1, "big")
 
 class Message:
-    def __init__(self, version=0, type_=None):
-        self.version = version
-        self.type = type_
+    """Dragonfly message"""
+
+    def __init__(self, origin=ORIGIN_SERVER, type_=CONNECT, flags=0, **kwargs):
+        """Initializes a Message instance
+
+        Keyword Arguments:
+            origin {int} -- The message's origin, either ORIGIN_SERVER or ORIGIN_CLIENT (default: {ORIGIN_SERVER})
+            type_ {int} -- The message's type, one of [CONNECT, CONNECTED, PUBLISH, PUBLISHED, SUBSCRIBE, SUBSCRIBED, UNSUBSCRIBE, UNSUBSCRIBED] (default: {CONNECT})
+            flags {int} -- The message's flags (default: {0})
+            **kwargs: additional message properties
+        """
+
+        self.bytes = b""
+        self.version = 0
+        self.type = MessageType(origin<<7 | type_<<4 | flags)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
     
     def __repr__(self):
         #return f"<v{self.version} Message of type {self.type}>"
         return str(self.__dict__)
     
     def from_bytes(self, bytes_):
+        """Parses a message from bytes
+
+        Arguments:
+            bytes_ {bytes} -- The message's bytes
+        """
+
+        self.bytes = bytes_
         stream = ByteStream(bytes_)
         self.version = struct.unpack(">H", stream.read(2))[0]
         self.type = MessageType(stream.read(1)[0])
@@ -66,40 +95,98 @@ class Message:
         if self.type.type == CONNECT:
             self.username, self.password = None, None
 
-            if self.type.flags & 2:
+            if self.type.flags & FLAG_1:
                 self.username = self.read_string(stream)
             
-            if self.type.flags & 1:
+            if self.type.flags & FLAG_0:
                 self.password = self.read_string(stream)
+
+        elif self.type.type == PUBLISH:
+            self.topic = self.read_string(stream)
+            self.body = self.read_string(stream)
+
+        elif self.type.type == SUBSCRIBE:
+            self.topic = self.read_string(stream)
+
+        elif self.type.type == UNSUBSCRIBE:
+            self.topic = self.read_string(stream)
+        
+        elif self.type.type in [PUBLISHED, SUBSCRIBED, UNSUBSCRIBED]:
+            self.code = struct.unpack(">B", stream.read(1))[0]
         
 
     def to_bytes(self):
+        """Formats the message to bytes
+
+        Returns:
+            bytes -- The message's bytes
+        """
+
         bytes_ = b""
 
         body = b""
 
         if self.type.type == CONNECT:
             if self.username:
-                self.type.flags |= 2
+                self.type.flags |= FLAG_1
                 body += self.write_string(self.username)
             
             if self.password:
-                self.type.flags |= 1
+                self.type.flags |= FLAG_0
                 body += self.write_string(self.password)
 
+        elif self.type.type == PUBLISH:
+            body += self.write_string(self.topic)
+            body += self.write_string(self.body)
+
+        elif self.type.type == SUBSCRIBE:
+            body += self.write_string(self.topic)
+
+        elif self.type.type == UNSUBSCRIBE:
+            body += self.write_string(self.topic)
+        
+        elif self.type.type in [PUBLISHED, SUBSCRIBED, UNSUBSCRIBED]:
+            body += struct.pack(">B", self.code)
 
         bytes_ += struct.pack(">H", self.version)
         bytes_ += self.type.to_bytes()
         bytes_ += struct.pack(">I", len(body))
         bytes_ += body
 
+        self.bytes = bytes_
         return bytes_
     
     def read_string(self, stream):
+        """Reads a string from a byte stream
+
+        A string is always preceeded by a 2 byte unsigned short indicating its
+        length.
+        Strings are decoded in UTF-8
+
+        Arguments:
+            stream {ByteStream} -- Input stream
+
+        Returns:
+            str -- Decoded string
+        """
+
         length = struct.unpack(">H", stream.read(2))[0]
         return stream.read(length).decode("utf-8")
     
     def write_string(self, string):
+        """Formats a string to bytes
+
+        A string is always preceeded by a 2 byte unsigned short indicating its
+        length.
+        Strings are encoded in UTF-8
+
+        Arguments:
+            string {str} -- String to encode
+
+        Returns:
+            bytes -- Encoded string
+        """
+
         bytes_ = struct.pack(">H", len(string))
         bytes_ += string.encode("utf-8")
         return bytes_
