ae85e0ca11ac4665964c3388df22137520feafba
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-06-29 12:21:15 +0200

    added serverside Client

--- src/dragonfly/server.py
+++ src/dragonfly/server.py
@@ -48,6 +48,8 @@ class Server:
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.selector = selectors.DefaultSelector()
+        self.clients = []
+        self.topics = {}
         self.state = State.STOPPED
     
     def start(self):
@@ -93,22 +95,25 @@ class Server:
         """
 
         conn, addr = sock.accept()
+        client = self.new_client(conn)
         Logger.debug(f"Accepted connection from {addr}")
         conn.setblocking(False)
-        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", step=0, length=0)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", step=0, length=0, id=client.id)
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
         self.selector.register(conn, events, data=data)
 
-    def close_conn(self, socket):
+    def close_conn(self, id_):
         """Closes a previously established connection
 
         Arguments:
-            socket {socket.socket} -- Socket to close
+            id_ {int} -- Client id
         """
 
-        Logger.debug(f"Closing connection {socket.getpeername()}")
-        self.selector.unregister(socket)
-        socket.close()
+        client = self.clients[id_]
+        Logger.debug(f"Closing connection {client.socket.getpeername()}")
+        self.selector.unregister(client.socket)
+        client.socket.close()
+        self.remove_client(id_)
 
     def handle_msg(self, key, mask):
         """Handles an event
@@ -134,17 +139,101 @@ class Server:
 
                 elif data.step == 2:
                     msg = Message()
-                    msg.from_bytes(data.outb)
-                    Logger.debug(f"Received {msg} from {data.addr}")
-                    data.step = 0
-                    data.outb = b""
+                    try:
+                        msg.from_bytes(data.outb)
+                    
+                    except:
+                        Logger.warn(f"Malformed packet from {data.addr}")
+                    
+                    else:
+                        Logger.debug(f"Received {msg} from {data.addr}")
+                    
+                    finally:
+                        data.step = 0
+                        data.outb = b""
             
             else:
-                self.close_conn(sock)
+                self.close_conn(data.id)
         
         # Ready to write
         if mask & selectors.EVENT_WRITE:
             pass
+    
+    def new_client(self, sock):
+        """Registers new client
+
+        Arguments:
+            sock {socket.socket} -- Client socket
+
+        Returns:
+            Client -- The new client instance
+        """
+
+        if not None in self.clients:
+            self.clients.append(None)
+        
+        id_ = self.clients.index(None)
+        client = Client(sock, id_)
+        self.clients[id_] = client
+
+        return client
+
+    def remove_client(self, id_):
+        """Unregisters a client
+
+        Arguments:
+            id_ {int} -- The client's id
+        """
+
+        client = self.clients[id_]
+        for topic in client.topics:
+            self.topics[topic].remove(id_)
+        
+        self.clients[id_] = None
+    
+class Client:
+    """Represents a client"""
+
+    def __init__(self, sock, id_):
+        """Initializes a Client instance
+
+        Arguments:
+            sock {socket.socket} -- The client socket
+            id_ {int} -- The client's id (see Server#new_client)
+        """
+
+        self.socket = sock
+        self.username = None
+        self.password = None
+        self.topics = []
+        self.id = id_
+    
+    def __repr__(self):
+        return f"<Client {self.id} topics={self.topics}>"
+    
+    def send(self, msg):
+        """Sends a message through the socket
+
+        Arguments:
+            msg {Message} -- Message to send
+        """
+
+        self.socket.sendall(msg.to_bytes())
+    
+    def check_auth(self, scope, *args):
+        """Checks user rights in `scope`
+
+        Arguments:
+            scope {int} -- Message type
+
+        Returns:
+            bool -- True if the client is authorized in this scope
+        
+        Todo:
+            * Write the function
+        """
+        
+        return True
 
 if __name__ == "__main__":
     Logger.setup(LogType.ALL)
