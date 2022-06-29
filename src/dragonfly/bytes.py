00a2abe196494ea0e1686b739aa9aeced51b87b1
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-06-29 12:23:35 +0200

    documented bytes.py

--- src/dragonfly/bytes.py
+++ src/dragonfly/bytes.py
@@ -19,14 +19,36 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 class ByteStream:
+    """Stream of bytes, simulates a file object"""
+
     def __init__(self, bytes_=b""):
+        """Initializes a ByteStream instance
+
+        Keyword Arguments:
+            bytes_ {bytes} -- Bytes string (default: {b""})
+        """
+
         self.bytes = bytes_
         self.pos = 0
     
     def clamp(self):
+        """Clamps the cursor's position to stay within the content's boundaries"""
+
         self.pos = min(len(self.bytes)-1, max(0, self.pos))
     
     def seek(self, offset, anchor=0):
+        """Changes stream position
+
+        Changes the stream position to the given byte offset. The offset is
+        interpreted relative to the position indicated by `anchor`.
+
+        Arguments:
+            offset {int} -- Byte offset
+
+        Keyword Arguments:
+            anchor {int} -- Offset anchor, 0: start / 1: current / 2: end (default: {0})
+        """
+
         if anchor == 0:
             self.pos = offset
         
@@ -39,6 +61,19 @@ class ByteStream:
         self.clamp()
     
     def read(self, count):
+        """Reads a certain amount of bytes from the stream
+
+        Arguments:
+            count {int} -- Number of bytes to read
+
+        Returns:
+            bytes -- Bytes read
+
+        Todo:
+            * Check for EOF
+            * Add default count=-1 -> read to end
+        """
+
         bytes_ = self.bytes[self.pos:self.pos+count]
         self.pos += count
         self.clamp()
