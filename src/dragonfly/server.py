c5a8d9f48dbc6e429814f465c9b32b9db3a674e8
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-07-03 16:54:34 +0200

    added socket option SO_REUSEADDR

--- src/dragonfly/server.py
+++ src/dragonfly/server.py
@@ -47,6 +47,7 @@ class Server:
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.selector = selectors.DefaultSelector()
         self.clients = []
         self.topics = {}
